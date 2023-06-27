7c1934e9be0c115ce6d4cf4186e781456f6ffdb7
wanyao <wanyaocsg17@icloud.com>
2023-06-27 11:05:56 +0800

    Update genotypes.py

--- genotypes.py
+++ genotypes.py
@@ -77,34 +77,40 @@ AmoebaNet = Genotype(
 
 # DARTS搜索到的cell结构
 DARTS_V1 = Genotype(
-    normal=[('sep_conv_3x3', 1), ('sep_conv_3x3', 0), ('skip_connect', 0), ('sep_conv_3x3', 1),
-            ('skip_connect', 0),
-            ('sep_conv_3x3', 1), ('sep_conv_3x3', 0), ('skip_connect', 2)],
+    normal=[('sep_conv_3x3', 1), ('sep_conv_3x3', 0), 
+            ('skip_connect', 0), ('sep_conv_3x3', 1),
+            ('skip_connect', 0), ('sep_conv_3x3', 1), 
+            ('sep_conv_3x3', 0), ('skip_connect', 2)],
     normal_concat=[2, 3, 4, 5],
-    reduce=[('max_pool_3x3', 0), ('max_pool_3x3', 1), ('skip_connect', 2), ('max_pool_3x3', 0),
-            ('max_pool_3x3', 0),
-            ('skip_connect', 2), ('skip_connect', 2), ('avg_pool_3x3', 0)],
+    reduce=[('max_pool_3x3', 0), ('max_pool_3x3', 1), 
+            ('skip_connect', 2), ('max_pool_3x3', 0),
+            ('max_pool_3x3', 0), ('skip_connect', 2), 
+            ('skip_connect', 2), ('avg_pool_3x3', 0)],
     reduce_concat=[2, 3, 4, 5]
 )
 DARTS_V2 = Genotype(
-    normal=[('sep_conv_3x3', 0), ('sep_conv_3x3', 1), ('sep_conv_3x3', 0), ('sep_conv_3x3', 1),
-            ('sep_conv_3x3', 1),
-            ('skip_connect', 0), ('skip_connect', 0), ('dil_conv_3x3', 2)],
+    normal=[('sep_conv_3x3', 0), ('sep_conv_3x3', 1), 
+            ('sep_conv_3x3', 0), ('sep_conv_3x3', 1),
+            ('sep_conv_3x3', 1), ('skip_connect', 0), 
+            ('skip_connect', 0), ('dil_conv_3x3', 2)],
     normal_concat=[2, 3, 4, 5],
-    reduce=[('max_pool_3x3', 0), ('max_pool_3x3', 1), ('skip_connect', 2), ('max_pool_3x3', 1),
-            ('max_pool_3x3', 0),
-            ('skip_connect', 2), ('skip_connect', 2), ('max_pool_3x3', 1)],
+    reduce=[('max_pool_3x3', 0), ('max_pool_3x3', 1), 
+            ('skip_connect', 2), ('max_pool_3x3', 1),
+            ('max_pool_3x3', 0), ('skip_connect', 2), 
+            ('skip_connect', 2), ('max_pool_3x3', 1)],
     reduce_concat=[2, 3, 4, 5]
 )
 
 MyDARTS = Genotype(
-    normal=[('max_pool_3x3', 0), ('max_pool_3x3', 1), ('max_pool_3x3', 0), ('max_pool_3x3', 2), 
-            ('max_pool_3x3', 2), 
-            ('max_pool_3x3', 0), ('max_pool_3x3', 2), ('max_pool_3x3', 0)], 
+    normal=[('max_pool_3x3', 0), ('max_pool_3x3', 1), 
+            ('max_pool_3x3', 0), ('max_pool_3x3', 2), 
+            ('max_pool_3x3', 2), ('max_pool_3x3', 0), 
+            ('max_pool_3x3', 2), ('max_pool_3x3', 0)], 
     normal_concat=range(2, 6), 
-    reduce=[('dil_conv_5x5', 1), ('dil_conv_5x5', 0), ('max_pool_3x3', 2), ('sep_conv_5x5', 1), 
-            ('dil_conv_5x5', 3), 
-            ('sep_conv_5x5', 0), ('dil_conv_5x5', 2), ('sep_conv_5x5', 3)], 
+    reduce=[('dil_conv_5x5', 1), ('dil_conv_5x5', 0), 
+            ('max_pool_3x3', 2), ('sep_conv_5x5', 1), 
+            ('dil_conv_5x5', 3), ('sep_conv_5x5', 0), 
+            ('dil_conv_5x5', 2), ('sep_conv_5x5', 3)], 
     reduce_concat=range(2, 6)
 )
 
