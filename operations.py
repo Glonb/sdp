18641c5a16c23b22b7d4b7ae904abaa87a937141
wanyao <wanyaocsg17@icloud.com>
2023-07-13 15:55:32 +0800

    Update operations.py

--- operations.py
+++ operations.py
@@ -131,7 +131,7 @@ class Zero(nn.Module):
     def forward(self, x):
         if self.stride == 1:
             return x.mul(0.)
-        return x[:, ::self.stride, :].mul(0.)
+        return x[:, :, ::self.stride].mul(0.)
 
 
 class FactorizedReduce(nn.Module):
