6c1b50d746b5eaa4d58c3bd13369f574d53fd53b
wanyao <wanyaocsg17@icloud.com>
2023-07-18 15:57:50 +0800

    Update genotypes.py

--- genotypes.py
+++ genotypes.py
@@ -101,29 +101,16 @@ DARTS_V2 = Genotype(
     reduce_concat=[2, 3, 4, 5]
 )
 
-MyDARTS = Genotype(
-    normal=[('max_pool_3x3', 0), ('max_pool_3x3', 1), 
-            ('max_pool_3x3', 0), ('max_pool_3x3', 2), 
-            ('max_pool_3x3', 2), ('max_pool_3x3', 0), 
-            ('max_pool_3x3', 2), ('max_pool_3x3', 0)], 
-    normal_concat=range(2, 6), 
-    reduce=[('dil_conv_5x5', 1), ('dil_conv_5x5', 0), 
-            ('max_pool_3x3', 2), ('sep_conv_5x5', 1), 
-            ('dil_conv_5x5', 3), ('sep_conv_5x5', 0), 
-            ('dil_conv_5x5', 2), ('sep_conv_5x5', 3)], 
-    reduce_concat=range(2, 6)
-)
-
 DARTS_MINE = Genotype(
-    normal=[('max_pool_3x3', 1), ('sep_conv_3x3', 0), 
-            ('sep_conv_5x5', 0), ('sep_conv_5x5', 1), 
-            ('dil_conv_5x5', 2), ('dil_conv_3x3', 3), 
-            ('max_pool_3x3', 2), ('dil_conv_3x3', 3)], 
+    normal=[('sep_conv_3x3', 1), ('dil_conv_3x3', 0), 
+            ('sep_conv_5x5', 2), ('sep_conv_5x5', 1),
+            ('dil_conv_5x5', 2), ('dil_conv_3x3', 1), 
+            ('dil_conv_3x3', 3), ('dil_conv_5x5', 1)],
     normal_concat=range(2, 6), 
-    reduce=[('dil_conv_5x5', 1), ('sep_conv_3x3', 0), 
-            ('dil_conv_5x5', 2), ('sep_conv_5x5', 1), 
-            ('sep_conv_5x5', 0), ('sep_conv_5x5', 3), 
-            ('dil_conv_3x3', 3), ('sep_conv_5x5', 4)], 
+    reduce=[('avg_pool_3x3', 0), ('sep_conv_3x3', 1), 
+            ('avg_pool_3x3', 0), ('dil_conv_3x3', 1), 
+            ('avg_pool_3x3', 0), ('dil_conv_3x3', 3), 
+            ('sep_conv_3x3', 0), ('dil_conv_5x5', 3)], 
     reduce_concat=range(2, 6)
 )
 
