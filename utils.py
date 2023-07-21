af118a60825c7e2434f20bbef68b976727173633
wanyao <wanyaocsg17@icloud.com>
2023-07-21 16:35:58 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -61,30 +61,6 @@ class Cutout:
         return img
 
 
-def _data_transforms_cifar10(args):
-    """
-    :param args:
-    :return:
-    """
-    CIFAR_MEAN = [0.49139968, 0.48215827, 0.44653124]
-    CIFAR_STD = [0.24703233, 0.24348505, 0.26158768]
-
-    train_transform = transforms.Compose([
-        transforms.RandomCrop(32, padding=4),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
-    ])
-    if args.cutout:
-        train_transform.transforms.append(Cutout(args.cutout_length))
-
-    valid_transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
-    ])
-    return train_transform, valid_transform
-
-
 def count_parameters_in_MB(model):
     """
     count all parameters excluding auxiliary
