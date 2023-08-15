dc6c9fdea8b6b8daf2cb6d3172ad6cf02d74e2bb
wanyao <wanyaocsg17@icloud.com>
2023-08-15 17:11:22 +0800

    Update train_search.py

--- train_search.py
+++ train_search.py
@@ -81,7 +81,7 @@ def main():
     # this is the optimizer to optimize
     optimizer = optim.SGD(model.parameters(), args.lr, momentum=args.momentum, weight_decay=args.wd)
 
-    train_data = MyDataset('/kaggle/input/sdp-data/xalan4_embed.npy', '/kaggle/input/sdp-data/xalan4_label.csv')
+    train_data = MyDataset('/kaggle/input/sdp-data/xalan6_embed.npy', '/kaggle/input/sdp-data/xalan6_label.csv')
 
     num_train = len(train_data) 
     indices = list(range(num_train))
