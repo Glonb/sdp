ff3e14657f3546f93e2d3f690f70e831fa78ece9
wanyao <wanyaocsg17@icloud.com>
2023-08-18 15:54:35 +0800

    Update genotypes.py

--- genotypes.py
+++ genotypes.py
@@ -20,17 +20,18 @@ PRIMITIVES = [
     # 'dil_conv_5'
 ]
 
-DARTS_MINE = Genotype(
-    normal=[('sep_conv_3', 1), ('dil_conv_3', 0), 
-            ('sep_conv_5', 2), ('sep_conv_5', 1),
-            ('dil_conv_5', 2), ('dil_conv_3', 1), 
-            ('dil_conv_3', 3), ('dil_conv_5', 1)],
+SDP_Genotype = Genotype(
+    normal=[('sep_conv_5', 1), ('sep_conv_5', 0), 
+            ('sep_conv_3', 2), ('sep_conv_3', 0), 
+            ('sep_conv_7', 3), ('sep_conv_3', 2), 
+            ('sep_conv_3', 3), ('sep_conv_3', 4)], 
     normal_concat=range(2, 6), 
-    reduce=[('avg_pool_3', 0), ('sep_conv_3', 1), 
-            ('avg_pool_3', 0), ('dil_conv_3', 1), 
-            ('avg_pool_3', 0), ('dil_conv_3', 3), 
-            ('sep_conv_3', 0), ('dil_conv_5', 3)], 
+    reduce=[('sep_conv_5', 1), ('sep_conv_3', 0), 
+            ('max_pool_3', 2), ('sep_conv_7', 1), 
+            ('sep_conv_7', 1), ('max_pool_3', 2), 
+            ('skip_connect', 1), ('max_pool_3', 2)], 
     reduce_concat=range(2, 6)
 )
 
-DARTS = DARTS_MINE
+
+DARTS = SDP_Genotype
