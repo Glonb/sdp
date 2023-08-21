dc74b49d7f9731c99997f31d1e845dc5bddf13fa
wanyao <wanyaocsg17@icloud.com>
2023-08-21 15:04:37 +0800

    Update test.py

--- test.py
+++ test.py
@@ -32,6 +32,10 @@ log_format = '%(asctime)s %(message)s'
 logging.basicConfig(stream=sys.stdout, level=logging.INFO,
                     format=log_format, datefmt='%m/%d %I:%M:%S %p')
 
+fh = logging.FileHandler(os.path.join(args.save, 'log.txt'))
+fh.setFormatter(logging.Formatter(log_format))
+logging.getLogger().addHandler(fh)
+
 def main():
 
     np.random.seed(args.seed)
