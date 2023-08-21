d7c42d3ade383f3f483dd8435c71cb85423fc64f
wanyao <wanyaocsg17@icloud.com>
2023-08-21 11:14:04 +0800

    Update train_search.py

--- train_search.py
+++ train_search.py
@@ -74,7 +74,7 @@ def main():
     logging.info("args = %s", args)
 
     # the pos_weight
-    pos_weight = torch.tensor([1])
+    pos_weight = torch.tensor([2])
 
     criterion = nn.BCEWithLogitsLoss(pos_weight = pos_weight).to(device)
     model = Network(args.init_ch, args.layers, criterion).to(device)
@@ -84,7 +84,7 @@ def main():
     # this is the optimizer to optimize
     optimizer = optim.SGD(model.parameters(), args.lr, momentum=args.momentum, weight_decay=args.wd)
 
-    train_data = MyDataset('/kaggle/input/sdp-data/xalan4_embed.npy', '/kaggle/input/sdp-data/xalan4_label.csv')
+    train_data = MyDataset('/kaggle/input/sdp-data/xalan5_embed.npy', '/kaggle/input/sdp-data/xalan5_label.csv')
 
     num_train = len(train_data) 
     indices = list(range(num_train))
