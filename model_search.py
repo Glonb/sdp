746b4fd5d1ee3b826ad95a57d4336152e7b06729
wanyao <wanyaocsg17@icloud.com>
2023-08-22 15:38:02 +0800

    Update model_search.py

--- model_search.py
+++ model_search.py
@@ -75,7 +75,7 @@ class Network(nn.Module):
 
     def new(self):
         
-        model_new = Network(self.c, self.layers, self.criterion).cuda()
+        model_new = Network(self.c, self.steps, self.criterion).cuda()
         for x, y in zip(model_new.arch_parameters(), self.arch_parameters()):
             x.data.copy_(y.data)
         return model_new
