8117a6e891ca6e01c3aac10ebdf1666a65c5573a
wanyao <wanyaocsg17@icloud.com>
2023-08-25 10:39:22 +0800

    Update operations.py

--- operations.py
+++ operations.py
@@ -6,13 +6,16 @@ import  torch.nn.functional as F
 
 OPS = {
     'none':         lambda C, stride, affine: Zero(stride),
+    'skip_connect': lambda C, stride, affine: Identity(),
     'avg_pool_3':   lambda C, stride, affine: nn.AvgPool1d(3, stride=stride, padding=1, count_include_pad=False),
     'avg_pool_5':   lambda C, stride, affine: nn.AvgPool1d(5, stride=stride, padding=2, count_include_pad=False),
     'avg_pool_7':   lambda C, stride, affine: nn.AvgPool1d(7, stride=stride, padding=3, count_include_pad=False),
     'max_pool_3':   lambda C, stride, affine: nn.MaxPool1d(3, stride=stride, padding=1),
     'max_pool_5':   lambda C, stride, affine: nn.MaxPool1d(5, stride=stride, padding=2),
     'max_pool_7':   lambda C, stride, affine: nn.MaxPool1d(7, stride=stride, padding=3),
-    'skip_connect': lambda C, stride, affine: Identity() if stride == 1 else FactorizedReduce(C, C, affine=affine),
+    'smp_conv_3':   lambda C, stride, affine: ReLUConvBN(C, C, 3, stride, 1, affine=affine),
+    'smp_conv_3':   lambda C, stride, affine: ReLUConvBN(C, C, 5, stride, 2, affine=affine),
+    'smp_conv_3':   lambda C, stride, affine: ReLUConvBN(C, C, 7, stride, 3, affine=affine),
     'sep_conv_3':   lambda C, stride, affine: SepConv(C, C, 3, stride, 1, affine=affine),
     'sep_conv_5':   lambda C, stride, affine: SepConv(C, C, 5, stride, 2, affine=affine),
     'sep_conv_7':   lambda C, stride, affine: SepConv(C, C, 7, stride, 3, affine=affine),
@@ -44,17 +47,13 @@ class DilConv(nn.Module):
     relu-dilated conv-bn
     """
     def __init__(self, C_in, C_out, kernel_size, stride, padding, dilation, affine=True):
-        """
-        :param padding: 2/4
-        :param dilation: 2
-        """
+        
         super(DilConv, self).__init__()
 
         self.op = nn.Sequential(
             nn.ReLU(inplace=False),
             nn.Conv1d(C_in, C_in, kernel_size=kernel_size, stride=stride, padding=padding,
-                      dilation=dilation,
-                      groups=C_in, bias=False),
+                      dilation=dilation, groups=C_in, bias=False),
             nn.Conv1d(C_in, C_out, kernel_size=1, padding=0, bias=False),
             nn.BatchNorm1d(C_out, affine=affine),
         )
@@ -68,19 +67,12 @@ class SepConv(nn.Module):
     implemented separate convolution via pytorch groups parameters
     """
     def __init__(self, C_in, C_out, kernel_size, stride, padding, affine=True):
-        """
-        :param padding: 1/2
-        """
+        
         super(SepConv, self).__init__()
 
         self.op = nn.Sequential(
             nn.ReLU(inplace=False),
             nn.Conv1d(C_in, C_in, kernel_size=kernel_size, stride=stride, padding=padding,
-                      groups=C_in, bias=False),
-            nn.Conv1d(C_in, C_in, kernel_size=1, padding=0, bias=False),
-            nn.BatchNorm1d(C_in, affine=affine),
-            nn.ReLU(inplace=False),
-            nn.Conv1d(C_in, C_in, kernel_size=kernel_size, stride=1, padding=padding,
                       groups=C_in, bias=False),
             nn.Conv1d(C_in, C_out, kernel_size=1, padding=0, bias=False),
             nn.BatchNorm1d(C_out, affine=affine),
@@ -111,23 +103,3 @@ class Zero(nn.Module):
         if self.stride == 1:
             return x.mul(0.)
         return x[:, :, ::self.stride].mul(0.)
-
-
-class FactorizedReduce(nn.Module):
-
-  def __init__(self, C_in, C_out, affine=True):
-    super(FactorizedReduce, self).__init__()
-    
-    assert C_out % 2 == 0
-    
-    self.relu = nn.ReLU(inplace=False)
-    self.conv_1 = nn.Conv1d(C_in, C_out // 2, 1, stride=2, padding=0, bias=False)
-    self.conv_2 = nn.Conv1d(C_in, C_out // 2, 1, stride=2, padding=0, bias=False)
-    self.bn = nn.BatchNorm1d(C_out, affine=affine)
-
-  def forward(self, x):
-    x = self.relu(x)
-    out = torch.cat([self.conv_1(x), self.conv_2(x[:,:,1:])], dim=1)
-    # print(out.shape)
-    out = self.bn(out)
-    return out
