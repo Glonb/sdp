5416bfc3ed240ce374438b34497dd0adeda4ce61
wanyao <wanyaocsg17@icloud.com>
2023-08-28 15:11:49 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -43,28 +43,6 @@ def metrics(output, target, threshold = 0.5):
     return  precision, recall, fpr, fnr, f1, g1, mcc
 
 
-class Cutout:
-    def __init__(self, length):
-        self.length = length
-
-    def __call__(self, img):
-        h, w = img.size(1), img.size(2)
-        mask = np.ones((h, w), np.float32)
-        y = np.random.randint(h)
-        x = np.random.randint(w)
-
-        y1 = np.clip(y - self.length // 2, 0, h)
-        y2 = np.clip(y + self.length // 2, 0, h)
-        x1 = np.clip(x - self.length // 2, 0, w)
-        x2 = np.clip(x + self.length // 2, 0, w)
-
-        mask[y1: y2, x1: x2] = 0.
-        mask = torch.from_numpy(mask)
-        mask = mask.expand_as(img)
-        img *= mask
-        return img
-
-
 def count_parameters_in_MB(model):
     """
     count all parameters excluding auxiliary
