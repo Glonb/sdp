824d64bc9a8043c8fe7a127918a3c474fa6c6fd9
wanyao <wanyaocsg17@icloud.com>
2023-08-28 15:41:07 +0800

    Update operations.py

--- operations.py
+++ operations.py
@@ -103,3 +103,11 @@ class Zero(nn.Module):
         if self.stride == 1:
             return x.mul(0.)
         return x[:, :, ::self.stride].mul(0.)
+
+class Flatten(nn.Module):
+    def __init__(self):
+        super(Flatten, self).__init__()
+        
+    def forward(self, x):
+        return x.view(x.size(0), -1)
+
