3349927a71e391ccd87e18c0edd8cb70a8151631
wanyao <wanyaocsg17@icloud.com>
2023-09-25 14:37:01 +0800

    Update train_search.py

--- train_search.py
+++ train_search.py
@@ -152,7 +152,7 @@ def train(train_queue, valid_queue, model, arch, criterion, optimizer, lr):
         # 2. update weight
         optimizer.zero_grad()
         loss.backward()
-        nn.utils.clip_grad_norm_(model.parameters(), args.grad_clip)
+        # nn.utils.clip_grad_norm_(model.parameters(), args.grad_clip)
         optimizer.step()
 
         prec, rec, FPR, FNR, f1, g1, MCC = utils.metrics(logits, target)
