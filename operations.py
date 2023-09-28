bf9798700f83a3eee9b59d079027d4d64fd18921
wanyao <wanyaocsg17@icloud.com>
2023-09-28 17:28:17 +0800

    Update operations.py

--- operations.py
+++ operations.py
@@ -5,35 +5,34 @@ import  torch.nn.functional as F
 
 
 OPS = {
-    'none':         lambda C, stride, affine: Zero(stride),
-    'skip_connect': lambda C, stride, affine: Identity(),
-    'avg_pool_3':   lambda C, stride, affine: nn.AvgPool1d(3, stride=stride, padding=1, count_include_pad=False),
-    'avg_pool_5':   lambda C, stride, affine: nn.AvgPool1d(5, stride=stride, padding=2, count_include_pad=False),
-    'avg_pool_7':   lambda C, stride, affine: nn.AvgPool1d(7, stride=stride, padding=3, count_include_pad=False),
-    'max_pool_3':   lambda C, stride, affine: nn.MaxPool1d(3, stride=stride, padding=1),
-    'max_pool_5':   lambda C, stride, affine: nn.MaxPool1d(5, stride=stride, padding=2),
-    'max_pool_7':   lambda C, stride, affine: nn.MaxPool1d(7, stride=stride, padding=3),
-    'conv_3':   lambda C_in, C_out, stride, affine: ReLUConvBN(C_in, C_out, 3, stride, 1, affine=affine),
-    'conv_5':   lambda C, stride, affine: ReLUConvBN(C, C, 5, stride, 2, affine=affine),
-    'conv_7':   lambda C, stride, affine: ReLUConvBN(C, C, 7, stride, 3, affine=affine),
-    'sep_conv_3':   lambda C, stride, affine: SepConv(C, C, 3, stride, 1, affine=affine),
-    'sep_conv_5':   lambda C, stride, affine: SepConv(C, C, 5, stride, 2, affine=affine),
-    'sep_conv_7':   lambda C, stride, affine: SepConv(C, C, 7, stride, 3, affine=affine),
-    'dil_conv_3':   lambda C, stride, affine: DilConv(C, C, 3, stride, 2, 2, affine=affine),
-    'dil_conv_5':   lambda C, stride, affine: DilConv(C, C, 5, stride, 4, 2, affine=affine)
+    # 'none':         lambda C, stride, affine: Zero(stride),
+    'skip_connect':  Identity(),
+    'conv_2':   lambda C_in, C_out, stride: ConvReLU(C_in, C_out, 3, stride, 1),
+    'conv_3':   lambda C_in, C_out, stride: ConvReLU(C_in, C_out, 4, stride, 1),
+    'conv_4':   lambda C_in, C_out, stride: ConvReLU(C_in, C_out, 5, stride, 1),
+    'avg_pool_2':    nn.AvgPool1d(2, stride=2, padding=0, count_include_pad=False),
+    'avg_pool_3':    nn.AvgPool1d(3, stride=2, padding=1, count_include_pad=False),
+    'max_pool_3':    nn.MaxPool1d(3, stride=2, padding=1),
+    'max_pool_4':    nn.MaxPool1d(4, stride=2, padding=1)
+    
+    # 'sep_conv_3':   lambda C, stride, affine: SepConv(C, C, 3, stride, 1, affine=affine),
+    # 'sep_conv_5':   lambda C, stride, affine: SepConv(C, C, 5, stride, 2, affine=affine),
+    # 'sep_conv_7':   lambda C, stride, affine: SepConv(C, C, 7, stride, 3, affine=affine),
+    # 'dil_conv_3':   lambda C, stride, affine: DilConv(C, C, 3, stride, 2, 2, affine=affine),
+    # 'dil_conv_5':   lambda C, stride, affine: DilConv(C, C, 5, stride, 4, 2, affine=affine)
 }
 
-class ConvBN(nn.Module):
+class ConvReLU(nn.Module):
     """
-    Stack of conv-bn
+    Stack of conv-relu
     """
-    def __init__(self, C_in, C_out, kernel_size, stride, padding, affine=True):
+    def __init__(self, C_in, C_out, kernel_size, stride, padding):
         
         super(ConvBN, self).__init__()
 
         self.op = nn.Sequential(
-            nn.Conv1d(C_in, C_out, kernel_size, stride=stride, padding=padding, bias=False),
-            nn.BatchNorm1d(C_out, affine=affine)
+            nn.Conv1d(C_in, C_out, kernel_size, stride=stride, padding=padding),
+            nn.ReLU()
         )
 
     def forward(self, x):
