b47f7f762fcf8d390dd9a13cbafd932c2afd39d4
wanyao <wanyaocsg17@icloud.com>
2023-10-07 10:28:38 +0800

    Update genotypes.py

--- genotypes.py
+++ genotypes.py
@@ -5,7 +5,6 @@ Genotype = namedtuple('Genotype', 'geno geno_concat')
 
 # 可选操作
 PRIMITIVES = [
-    'none',
     'max_pool_3',
     'max_pool_5',
     'max_pool_7',
@@ -13,14 +12,14 @@ PRIMITIVES = [
     'avg_pool_5',
     'avg_pool_7',
     'skip_connect',
-    'smp_conv_3',
-    'smp_conv_5',
-    'smp_conv_7',
-    'sep_conv_3',
-    'sep_conv_5',
-    'sep_conv_7',
-    'dil_conv_3',
-    'dil_conv_5'
+    'conv_3',
+    'conv_5',
+    'conv_7',
+    # 'sep_conv_3',
+    # 'sep_conv_5',
+    # 'sep_conv_7',
+    # 'dil_conv_3',
+    # 'dil_conv_5'
 ]
 
 SDP_Genotype = Genotype(
