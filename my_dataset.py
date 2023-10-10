b7806dfb59c4f858d5c54a3f06f6d01043300344
wanyao <wanyaocsg17@icloud.com>
2023-10-10 16:22:21 +0800

    Add files via upload

--- my_dataset.py
+++ my_dataset.py
@@ -1,21 +1,18 @@
 import torch
 from torch.utils.data import Dataset
-import numpy as np
-import pandas as pd
 
 
 class MyDataset(Dataset):
 
-    def __init__(self, numpy_file, label_file):
-        data = np.load(numpy_file)
-        # 转化为FloatTensor类型
-        self.data = torch.from_numpy(data).float()
-        labels = pd.read_csv(label_file, usecols=['bug']).values
-        self.labels = torch.from_numpy(labels)
+    def __init__(self, file_path):
+        self.data = torch.load(file_path)
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        return self.data[idx], self.labels[idx]
+        item = self.data[idx]
+        token_ids = item['token_ids']
+        label = item['label']
+        return token_ids, label
 
