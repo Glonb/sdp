cd5328a8e50dec6d201f69d1b3da3f8cba2318a4
wanyao <wanyaocsg17@icloud.com>
2023-10-11 21:26:28 +0800

    Update test.py

--- test.py
+++ test.py
@@ -31,7 +31,7 @@ log_format = '%(asctime)s %(message)s'
 logging.basicConfig(stream=sys.stdout, level=logging.INFO,
                     format=log_format, datefmt='%m/%d %I:%M:%S %p')
 
-fh = logging.FileHandler(os.path.join(args.save, 'log.txt'))
+fh = logging.FileHandler('sdp-test/log.txt')
 fh.setFormatter(logging.Formatter(log_format))
 logging.getLogger().addHandler(fh)
 
