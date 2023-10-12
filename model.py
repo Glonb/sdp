02a3213a5c64fd72228561d9023e22267b46a6a3
wanyao <wanyaocsg17@icloud.com>
2023-10-12 19:09:56 +0800

    Update model.py

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ class Network(nn.Module):
         super(Network, self).__init__()
         self.dropout_prob = dropout_prob
         self.vocab_size = vocab_size
-        hidden_size = 32
+        hidden_size = 64
 
         op_names, indices = zip(*genotype.geno)
         concat = genotype.geno_concat
