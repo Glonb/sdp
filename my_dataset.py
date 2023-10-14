63908019faed7199328a8a99605d157b185eaf90
wanyao <wanyaocsg17@icloud.com>
2023-10-14 20:26:22 +0800

    Update my_dataset.py

--- my_dataset.py
+++ my_dataset.py
@@ -1,18 +1,17 @@
 import torch
 from torch.utils.data import Dataset
+import pandas as pd
 
 
 class MyDataset(Dataset):
 
-    def __init__(self, file_path):
-        self.data = torch.load(file_path)
+    def __init__(self, embed_file, label_file):
+        self.data = torch.load(embed_file)
+        labels = pd.read_csv(label_file, usecols=['bug']).values
+        self.labels = torch.tensor(labels)
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        item = self.data[idx]
-        token_ids = item['token_ids']
-        label = item['label']
-        return token_ids, label
-
+        return self.data[idx], self.labels[idx]
