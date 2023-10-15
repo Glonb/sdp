9a058192a51fd610ccd495219d7030c7395ed3a0
wanyao <wanyaocsg17@icloud.com>
2023-10-15 15:41:14 +0800

    Update model.py

--- model.py
+++ model.py
@@ -5,27 +5,18 @@ from    operations import *
 
 class Network(nn.Module):
 
-    def __init__(self, C, dropout_prob, vocab_size, genotype):
+    def __init__(self, C, hidden_size, genotype):
         super(Network, self).__init__()
-        self.dropout_prob = dropout_prob
-        self.vocab_size = vocab_size
-        hidden_size = 64
+        
+        self.hidden_size = hidden_size
+        out_dim = C + 2 * hidden_size
 
         op_names, indices = zip(*genotype.geno)
         concat = genotype.geno_concat
         self._compile(C, op_names, indices, concat)
 
-        self.embed = nn.Embedding(vocab_size, C)
-        self.bilstm = nn.LSTM(input_size=C, hidden_size=hidden_size, bidirectional=True, batch_first=True)
+        self.bilstm = nn.LSTM(input_size=C, hidden_size=self.hidden_size, bidirectional=True, batch_first=True)
         self.global_pooling = nn.AdaptiveMaxPool1d(1)
-        
-        out_dim = C * 2 + 2 * hidden_size
-        hidden_dim = out_dim // 2
-        
-        # self.fc1 = nn.Linear(out_dim, hidden_dim)
-        # self.dropout = nn.Dropout(p=self.dropout_prob)
-        # self.fc2 = nn.Linear(hidden_dim, 1)
-
         self.fc = nn.Linear(out_dim, 1)
 
     def _compile(self, C, op_names, indices, concat):
@@ -37,16 +28,14 @@ class Network(nn.Module):
 
         self._ops = nn.ModuleList()
         for name, index in zip(op_names, indices):
-            stride = 1
-            op = OPS[name](C, stride)
+            op = OPS[name](C)
             self._ops += [op]
         self._indices = indices
 
     def forward(self, x):
-
-        x = self.embed(x)
+        
         input = x.permute(0, 2, 1)
-        states = [input]
+        states = [x]
         
         for i in range(self._steps):
             h = states[self._indices[i]]
@@ -54,18 +43,20 @@ class Network(nn.Module):
             h = op(h)
             states += [h]
             
-        cnn_out = torch.cat(states[-2:], dim=1)
-        # cnn_out = states[-1]
+        # cnn_out = torch.cat(states[-2:], dim=1)
+        
+        cnn_out = states[-1]
         cnn_out = self.global_pooling(cnn_out)
+        cnn_out = cnn_out.view(cnn_out.size(0), -1)
 
-        bilstm_out, (h_n, c_n) = self.bilstm(x)
-        bilstm_out = bilstm_out.transpose(1, 2)
-        bilstm_out = self.global_pooling(bilstm_out)
+        bilstm_out, (h_n, c_n) = self.bilstm(input)
+        forward_state, backward_state = h_n[0], h_n[1]
 
-        out = torch.cat([cnn_out, bilstm_out], dim=1)
-        out = out.view(out.size(0), -1)
-        # fc1_out = self.fc1(out)    
-        # out_drop = self.dropout(fc1_out)
-        # logits = self.fc2(out_drop)
+        combined_state = torch.cat((forward_state, backward_state), dim=-1)
+        bilstm_out = combined_state.permute(1, 0, 2)
+        bilstm_out = bilstm_out.view(bilstm_out.size(0), -1)
+
+        out = torch.cat([cnn_out, bilstm_out], dim=-1)
         logits = self.fc(out)
-        return torch.sigmoid(logits).view(-1)
+        
+        return torch.sigmoid(logits)
