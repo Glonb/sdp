17fe71c98dc19ffa6deef558a15966c3dd02d165
wanyao <wanyaocsg17@icloud.com>
2023-10-16 19:47:51 +0800

    Update model.py

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ class Network(nn.Module):
         super(Network, self).__init__()
         
         self.hidden_size = hidden_size
-        out_dim = C * 2 + 2 * hidden_size + 18
+        out_dim = C * 4 + 2 * hidden_size + 18
 
         op_names, indices = zip(*genotype.geno)
         concat = genotype.geno_concat
@@ -44,7 +44,9 @@ class Network(nn.Module):
             states += [h]
             
         # cnn_out = torch.cat(states[-2:], dim=1)
-        cnn_out = torch.cat((self.global_pooling(states[-1]),self.global_pooling(states[-2])), dim = 1)
+        # cnn_out = torch.cat((self.global_pooling(states[-1]),self.global_pooling(states[-2])), dim = 1)
+        pooled_states = [self.global_pooling(h) for h in states[1:]]
+        cnn_out = torch.cat(pooled_states, dim=1)
         
         # cnn_out = states[-1]
         # cnn_out = self.global_pooling(cnn_out)
