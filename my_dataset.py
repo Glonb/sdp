c2f9a35856ebd534b21d6fb1cd5fcd2af2e3263b
wanyao <wanyaocsg17@icloud.com>
2023-10-16 16:34:32 +0800

    Update my_dataset.py

--- my_dataset.py
+++ my_dataset.py
@@ -14,7 +14,7 @@ class MyDataset(Dataset):
 
     def __getitem__(self, idx):
         tr_features = torch.tensor(self.csv_data.iloc[idx, 1:-1], dtype=torch.float32)
-        label = torch.tensor(self.csv_data.iloc[idx, -1], dtype=torch.long)
+        label = torch.tensor(self.csv_data.iloc[idx, -1], dtype=torch.long).unsqueeze(0)
 
         return self.emb_data[idx].float(), tr_features, label
 
