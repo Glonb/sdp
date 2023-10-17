154757ab281261ca4fef2a8e330903e7ee7ac5e8
wanyao <wanyaocsg17@icloud.com>
2023-10-17 19:18:10 +0800

    Update model_search.py

--- model_search.py
+++ model_search.py
@@ -89,7 +89,12 @@ class Network(nn.Module):
         for i in range(self.steps):
             
             weights = F.softmax(self.alpha, dim=-1)
-            s = sum(self.layers[offset + j](h, weights[offset + j]) for j, h in enumerate(states))
+            # s = sum(self.layers[offset + j](h, weights[offset + j]) for j, h in enumerate(states))
+            s = self.layers[offset + j](h, weights[offset + j]) for j, h in enumerate(states)
+            print(tensor.shape for tensor in s)
+            # max_length = max(tensor.size(-1) for tensor in s)
+            # padded_tensors = [F.pad(tensor, (0, max_length - tensor.size(-1))) for tensor in s]
+            # s = torch.stack(padded_tensors, dim=-1).sum(dim=-1)
             offset += len(states)
             
             # append one state since s is the elem-wise addition of all output
