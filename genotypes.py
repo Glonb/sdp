47208ba69bccf1fc1780dea39d7b3c12bb2dc549
wanyao <wanyaocsg17@icloud.com>
2023-10-24 16:11:25 +0800

    Update genotypes.py

--- genotypes.py
+++ genotypes.py
@@ -36,10 +36,10 @@ PRIMITIVES = [
 ]
 
 SDP_Genotype = Genotype(
-    geno=[('conv_7_1', 0),
-          ('conv_7_1', 0),
-          ('conv_7_1', 0), ('skip_connect', 2),
-          ('conv_7_1', 0), ('conv_6_1', 1)],
+    geno=[('skip_connect', 0),
+          ('skip_connect', 0),
+          ('conv_6_1', 0), ('skip_connect', 2),
+          ('conv_7_1', 0), ('skip_connect', 3)],
     geno_concat=range(1, 5)
 )
 SDP = SDP_Genotype
