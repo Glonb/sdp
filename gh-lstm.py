33ad02370253949c94f9ecac224b1ceb03bc5e25
wanyao <wanyaocsg17@icloud.com>
2023-10-30 16:46:43 +0800

    Update gh-lstm.py

--- gh-lstm.py
+++ gh-lstm.py
@@ -70,8 +70,8 @@ criterion = nn.BCEWithLogitsLoss(pos_weight=pos_weight)
 # 定义优化器
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-train_data = MyDataset('/kaggle/input/sdp-own/poi15_train.pt', '/kaggle/input/sdp-own/poi15.csv')
-test_data = MyDataset('/kaggle/input/sdp-own/poi25_test.pt', '/kaggle/input/sdp-own/poi25.csv')
+train_data = MyDataset('/kaggle/input/sdp-own/poi25_train.pt', '/kaggle/input/sdp-own/poi25.csv')
+test_data = MyDataset('/kaggle/input/sdp-own/poi30_test.pt', '/kaggle/input/sdp-own/poi30.csv')
 
 batch_size = 2048
 train_dataloader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
