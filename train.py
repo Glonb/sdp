4c9f0330f4a4b0e478e92489ea3e87820586c440
wanyao <wanyaocsg17@icloud.com>
2023-10-30 10:45:48 +0800

    Update train.py

--- train.py
+++ train.py
@@ -79,7 +79,7 @@ def main():
 
     logging.info("param size = %fMB", utils.count_parameters_in_MB(model))
 
-    pos_weight = torch.tensor(0.6)
+    pos_weight = torch.tensor(0.5)
     criterion = nn.BCEWithLogitsLoss(pos_weight=pos_weight).cuda()
   
     # optimizer = torch.optim.SGD(model.parameters(), args.lr, momentum=args.momentum, weight_decay=args.wd)
