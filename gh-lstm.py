5683f720c3f796e8fa26fba3a582aae5e239d771
wanyao <wanyaocsg17@icloud.com>
2023-11-01 14:48:24 +0800

    Update gh-lstm.py

--- gh-lstm.py
+++ gh-lstm.py
@@ -16,6 +16,7 @@ else:
 
 
 def my_loss(y_pred, y_true):
+    print(y_pred)
     margin = 0.6
 
     # Define theta function
@@ -28,7 +29,7 @@ def my_loss(y_pred, y_true):
         - theta(1 - margin - y_true) * theta(1 - margin - y_pred)) * 
         (y_true * torch.log(y_pred + 1e-8) + (1 - y_true) * torch.log(1 - y_pred + 1e-8))
     )
-    print(loss)
+    
     return loss.mean()  # You can use .mean() to compute the average loss
     
 
