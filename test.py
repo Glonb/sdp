f51334fb361a47db9c9529155c7bd43da260e7a2
wanyao <wanyaocsg17@icloud.com>
2023-11-01 15:39:28 +0800

    Update test.py

--- test.py
+++ test.py
@@ -59,8 +59,8 @@ def main():
 
     logging.info("param size = %fMB", utils.count_parameters_in_MB(model))
 
-    # criterion = nn.BCEWithLogitsLoss().cuda()
-    criterion = my_loss
+    criterion = nn.BCEWithLogitsLoss().cuda()
+    # criterion = my_loss
   
     test_prec, test_rec, test_f1 = infer(test_queue, model, criterion)
     
