bc53c017d8c1596655b6d3ab73856f5fd91c5a56
wanyao <wanyaocsg17@icloud.com>
2023-11-07 11:05:40 +0800

    Update model_search.py

--- model_search.py
+++ model_search.py
@@ -44,7 +44,7 @@ class Network(nn.Module):
         self.criterion = criterion
         
         # out_dim = c * 2 + 2 * hidden_size + 48
-        out_dim = 168
+        out_dim = 208
         
         self.layers = nn.ModuleList()
 
@@ -105,10 +105,10 @@ class Network(nn.Module):
             # append one state since s is the elem-wise addition of all output
             states.append(s)
 
-        # pooled_states = [self.global_pooling(h) for h in states[-2:]]
-        # cnn_out = torch.cat(pooled_states, dim=-1)
+        pooled_states = [self.global_pooling(h) for h in states[-2:]]
+        cnn_out = torch.cat(pooled_states, dim=-1)
         
-        cnn_out = self.global_pooling(states[-1])
+        # cnn_out = self.global_pooling(states[-1])
         
         cnn_out = cnn_out.view(cnn_out.size(0), -1)
         # cnn_gate_out = self.sigmoid(self.cnn_gate(cnn_out))
@@ -151,7 +151,7 @@ class Network(nn.Module):
             n = 1
             start = 0
             for i in range(self.steps): # for each node
-                idx = 1
+                idx = i // 2 + 1
                 end = start + n
                 W = weights[start:end].copy()
                 edges = sorted(range(i + 1), # i+1 is the number of connection for node i
