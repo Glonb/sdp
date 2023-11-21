5292a779dce1d3ba0ecefe335b3d422ff81c25d3
wanyao <wanyaocsg17@icloud.com>
2023-11-21 11:01:41 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -28,7 +28,7 @@ for key in "${!versions[@]}"; do
     for ((i=0; i<${#values_array[@]}-1; i++)); do
         value1=${values_array[$i]}
         value2=${values_array[$i+1]}
-        echo "$key_$value1_$value2 start!"
+        echo "${key}_${value1}_${value2} start!"
         python gh-lstm.py --train_data "$key-$value1" --test_data "$key-$value2" --batchsz 256
     done
 done
