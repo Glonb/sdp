2916eda6ca49cc1266aa4477e422ac90f2d95141
wanyao <wanyaocsg17@icloud.com>
2023-11-21 16:11:52 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,18 +3,31 @@
 # projects=('ant', 'camel', 'ivy', 'jedit', 'log4j', 'lucene', 'poi', 'synapse', 'velocity', 'xalan', 'xerces')
 
 declare -A versions=(
-    ['ant']='1.3 1.4 1.5 1.6 1.7'
+    ['ant']='1.5 1.6 1.7'
     ['camel']='1.2 1.4 1.6'
     ['ivy']='1.4 2.0'
-    ['jedit']='3.2 4.0 4.1 4.2'
+    ['jedit']='3.2 4.0 4.1'
     ['log4j']='1.0 1.1'
     ['lucene']='2.0 2.2 2.4'
-    ['poi']='1.5 2.0 2.5 3.0'
+    ['poi']='1.5 2.5 3.0'
     ['synapse']='1.0 1.1 1.2'
-    ['velocity']='1.4 1.5 1.6'
-    ['xalan']='2.4 2.5 2.6'
-    ['xerces']='1.1 1.2 1.3'
+    ['xalan']='2.4 2.5'
+    ['xerces']='1.2 1.3'
 )
+
+# declare -A versions=(
+#     ['ant']='1.3 1.4 1.5 1.6 1.7'
+#     ['camel']='1.2 1.4 1.6'
+#     ['ivy']='1.4 2.0'
+#     ['jedit']='3.2 4.0 4.1 4.2'
+#     ['log4j']='1.0 1.1'
+#     ['lucene']='2.0 2.2 2.4'
+#     ['poi']='1.5 2.0 2.5 3.0'
+#     ['synapse']='1.0 1.1 1.2'
+#     ['velocity']='1.4 1.5 1.6'
+#     ['xalan']='2.4 2.5 2.6'
+#     ['xerces']='1.1 1.2 1.3'
+# )
 
 # 遍历关联数组的键
 for key in "${!versions[@]}"; do
