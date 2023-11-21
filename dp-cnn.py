860e1f820766297416eafb3ec3912674230de02d
wanyao <wanyaocsg17@icloud.com>
2023-11-21 12:21:45 +0800

    Update dp-cnn.py

--- dp-cnn.py
+++ dp-cnn.py
@@ -3,7 +3,7 @@ import    torch.nn as nn
 import    torch.optim as optim
 import    pandas as pd
 import    utils
-import    time
+import    time,os,sys,logging
 import    argparse
 from      torch.utils.data import DataLoader
 from      my_dataset import MyDataset
@@ -14,8 +14,18 @@ parser.add_argument('--train_data', type=str, default='ant-1.5', help='train dat
 parser.add_argument('--test_data', type=str, default='ant-1.6', help='test dataset')
 parser.add_argument('--input_dim', type=int, default=40, help='input dim')
 parser.add_argument('--batchsz', type=int, default=32, help='batch size')
+parser.add_argument('--log_path', type=str, default='dp-cnn', help='log path')
 parser.add_argument('--epochs', type=int, default=15, help='num of training epochs')
 args = parser.parse_args()
+
+utils.create_exp_dir(args.log_path)
+
+log_format = '%(message)s'
+logging.basicConfig(stream=sys.stdout, level=logging.INFO,
+                    format=log_format)
+fh = logging.FileHandler(os.path.join(args.log_path, 'log.txt'))
+fh.setFormatter(logging.Formatter(log_format))
+logging.getLogger().addHandler(fh)
 
 # 检查 GPU 可用性
 if torch.cuda.is_available():
@@ -78,6 +88,7 @@ test_data = MyDataset(data_loc + args.test_data + '_test.pt', data_loc + args.te
 
 train_dataloader = DataLoader(train_data, batch_size=args.batchsz, shuffle=True)
 test_dataloader = DataLoader(test_data, batch_size=args.batchsz, shuffle=True)
+logging.info('_________________________________Project %s -> %s________________________________', train_data, test_data)
 
 df = pd.read_csv(data_loc + args.train_data + '.csv')
 labels = df['bug'].values.reshape(-1, 1)
@@ -103,6 +114,8 @@ for epoch in range(args.epochs):
     losses = utils.AverageMeter()
     precision = utils.AverageMeter()
     recall = utils.AverageMeter()
+    fpr = utils.AverageMeter()
+    fnr = utils.AverageMeter()
     f_measure = utils.AverageMeter()
     g_measure = utils.AverageMeter()
     mcc = utils.AverageMeter()
@@ -125,16 +138,22 @@ for epoch in range(args.epochs):
         losses.update(loss.item(), args.batchsz)
         precision.update(prec, args.batchsz)
         recall.update(rec, args.batchsz)
+        fpr.update(FPR, args.batchsz)
+        fnr.update(FNR, args.batchsz)
         f_measure.update(f1, args.batchsz)
         g_measure.update(g1, args.batchsz)
         mcc.update(MCC, args.batchsz)
 
-    print(f'Epoch {epoch + 1}/{args.epochs}, Loss: {losses.avg:.3f}, Precision: {precision.avg:.3f}, Recall: {recall.avg:.3f}, F1 Score: {f_measure.avg:.3f}')
+    logging.info('Epoch:%03d loss:%.3f prec:%.3f recall:%.3f fpr:%.3f fnr:%.3f f1:%.3f g1:%.3f mcc:%.3f', 
+                         epoch+1, losses.avg, precision.avg, recall.avg, fpr.avg, 
+                         fnr.avg, f_measure.avg, g_measure.avg, mcc.avg)
+    # print(f'Epoch {epoch + 1}/{args.epochs}, Loss: {losses.avg:.3f}, Precision: {precision.avg:.3f}, Recall: {recall.avg:.3f}, F1 Score: {f_measure.avg:.3f}')
 
 end_training_time = time.time()
 
 training_time = end_training_time - start_training_time
-print(f"模型训练时间：{training_time}秒")
+logging.info('Train time:%.3fs', training_time)
+# print(f"模型训练时间：{training_time}秒")
 
 start_testing_time = time.time()
 
@@ -143,6 +162,8 @@ with torch.no_grad():
     losses = utils.AverageMeter()
     precision = utils.AverageMeter()
     recall = utils.AverageMeter()
+    fpr = utils.AverageMeter()
+    fnr = utils.AverageMeter()
     f_measure = utils.AverageMeter()
     g_measure = utils.AverageMeter()
     mcc = utils.AverageMeter()
@@ -161,13 +182,19 @@ with torch.no_grad():
         losses.update(loss.item(), args.batchsz)
         precision.update(prec, args.batchsz)
         recall.update(rec, args.batchsz)
+        fpr.update(FPR, args.batchsz)
+        fnr.update(FNR, args.batchsz)
         f_measure.update(f1, args.batchsz)
         g_measure.update(g1, args.batchsz)
         mcc.update(MCC, args.batchsz)
 
-    print(f'Test Loss: {losses.avg:.3f}, Precision: {precision.avg:.3f}, Recall: {recall.avg:.3f}, F1: {f_measure.avg:.3f}, G1: {g_measure.avg:.3f}, MCC: {mcc.avg:.3f}')
+    logging.info('Test loss:%.3f prec:%.3f recall:%.3f fpr:%.3f fnr:%.3f f1:%.3f g1:%.3f mcc:%.3f', 
+                         losses.avg, precision.avg, recall.avg, fpr.avg, 
+                         fnr.avg, f_measure.avg, g_measure.avg, mcc.avg)
+    print(f'Test Loss: {losses.avg:.3f}, Precision: {precision.avg:.3f}, Recall: {recall.avg:.3f}, F1: {f_measure.avg:.3f}')
 
 end_testing_time = time.time()
 
 testing_time = end_testing_time - start_testing_time
-print(f"模型测试时间：{testing_time}秒")
+logging.info('Test time:%.3fs', testing_time)
+# print(f"模型测试时间：{testing_time}秒")
