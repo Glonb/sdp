9b75c3bd2042e484eb09d252b020b8bcc330aa44
wanyao <wanyaocsg17@icloud.com>
2023-11-21 11:47:45 +0800

    Update gh-lstm.py

--- gh-lstm.py
+++ gh-lstm.py
@@ -90,7 +90,7 @@ test_data = MyDataset(data_loc + args.test_data + '_test.pt', data_loc + args.te
 
 train_dataloader = DataLoader(train_data, batch_size=args.batchsz, shuffle=True)
 test_dataloader = DataLoader(test_data, batch_size=args.batchsz, shuffle=True)
-logging.info('________________________Project %s -> %s________________________', args.train_data, args.test_data)
+logging.info('________________________________Project %s -> %s________________________________', args.train_data, args.test_data)
 
 # 创建模型实例
 model = MyModel(input_dim=args.input_dim, hidden_dim=128).to(device)
