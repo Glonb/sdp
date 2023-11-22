03b4cdc1080d404daf6b708e3b1ee910510a29fb
wanyao <wanyaocsg17@icloud.com>
2023-11-22 19:13:36 +0800

    Update genotypes.py

--- genotypes.py
+++ genotypes.py
@@ -19,11 +19,11 @@ PRIMITIVES = [
     'conv_7_1',
     'conv_9_1',
     'conv_3_2',
-    'conv_4_2',
     'conv_5_2',
-    'conv_6_2',
     'conv_7_2',
     'conv_9_2',
+    'conv_7_3',
+    'conv_9_3'
     # 'sep_conv_3_1',
     # 'sep_conv_5_1',
     # 'sep_conv_7_1',
