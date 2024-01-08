65fb827b0bee7ca9204d4715cbbe533922fc5e87
wanyao <wanyaocsg17@icloud.com>
2024-01-08 15:58:35 +0800

    Update operations.py

--- operations.py
+++ operations.py
@@ -19,17 +19,6 @@ OPS = {
     'conv_7_2':        lambda C: ConvReLU(C, C, 7, 2),
     'conv_5_3':        lambda C: ConvReLU(C, C, 5, 3),
     'conv_7_3':        lambda C: ConvReLU(C, C, 7, 3),
-    # 'sep_conv_3_1':    lambda C: SepConv(C, C, 3, 1),
-    # 'sep_conv_5_1':    lambda C: SepConv(C, C, 5, 1),
-    # 'sep_conv_7_1':    lambda C: SepConv(C, C, 7, 1),
-    # 'sep_conv_9_1':    lambda C: SepConv(C, C, 9, 1),
-    # 'sep_conv_3_2':    lambda C: SepConv(C, C, 3, 2),
-    # 'sep_conv_5_2':    lambda C: SepConv(C, C, 5, 2),
-    # 'sep_conv_7_2':    lambda C: SepConv(C, C, 7, 2),
-    # 'sep_conv_9_2':    lambda C: SepConv(C, C, 9, 2),
-    # 'dil_conv_3_1':    lambda C: DilConv(C, C, 3, 1, 2),
-    # 'dil_conv_5_1':    lambda C: DilConv(C, C, 5, 1, 2),
-    # 'dil_conv_7_1':    lambda C: DilConv(C, C, 7, 1, 2),
     'avg_pool_3':      lambda C: nn.AvgPool1d(kernel_size=3, stride=2, padding=1, count_include_pad=False),
     'avg_pool_5':      lambda C: nn.AvgPool1d(kernel_size=5, stride=2, padding=2, count_include_pad=False),
     'avg_pool_7':      lambda C: nn.AvgPool1d(kernel_size=7, stride=2, padding=3, count_include_pad=False),
@@ -61,76 +50,6 @@ class ConvReLU(nn.Module):
         return self.op(x)
 
 
-class DilConv(nn.Module):
-    """
-    dilated conv-relu
-    """
-    def __init__(self, C_in, C_out, kernel_size, stride, dilation):
-        
-        super(DilConv, self).__init__()
-
-        self.op = nn.Sequential(
-            nn.Conv1d(C_in, C_in, kernel_size=kernel_size, stride=stride, dilation=dilation, groups=C_in),
-            nn.Conv1d(C_in, C_out, kernel_size=1, padding=0),
-            nn.ReLU()
-        )
-
-    def forward(self, x):
-        return self.op(x)
-
-
-class SepConv(nn.Module):
-    """
-    implemented separate convolution via pytorch groups parameters
-    """
-    def __init__(self, C_in, C_out, kernel_size, stride):
-        
-        super(SepConv, self).__init__()
-
-        self.op = nn.Sequential(
-            nn.Conv1d(C_in, C_in, kernel_size=kernel_size, stride=stride, groups=C_in),
-            nn.Conv1d(C_in, C_out, kernel_size=1, padding=0),
-            nn.ReLU()
-        )
-
-    def forward(self, x):
-        return self.op(x)
-
-
-# class AvgPool(nn.Module):
-    
-#     def __init__(self, kernel_size, stride, padding):
-        
-#         super(AvgPool, self).__init__()
-
-#         self.op = nn.AvgPool1d(kernel_size=kernel_size, stride=2*stride, padding=padding, count_include_pad=False)
-           
-#     def forward(self, x):
-#         x = self.op(x)
-#         batch_size, channels, length = x.size()
-
-#         padding_layer = nn.ConstantPad1d(padding=(0, length), value=0.0)
-        
-#         return padding_layer(x)
-
-
-# class MaxPoolPadding(nn.Module):
-    
-#     def __init__(self, kernel_size, stride, padding):
-        
-#         super(MaxPoolPadding, self).__init__()
-        
-#         self.op = nn.MaxPool1d(kernel_size=kernel_size, stride=2*stride, padding=padding)
-
-#     def forward(self, x):
-#         x = self.op(x)
-#         batch_size, channels, length = x.size()
-        
-#         padding_layer = nn.ConstantPad1d(padding=(0, length), value=0.0)
-        
-#         return padding_layer(x)
-
-
 class Identity(nn.Module):
 
     def __init__(self):
@@ -138,6 +57,3 @@ class Identity(nn.Module):
 
     def forward(self, x):
         return x
-
-
-
