b7d279f1d3b3bec19e9c60c2105e9d23e1f3b039
wanyao <wanyaocsg17@icloud.com>
2024-01-15 10:29:39 +0800

    Update operations.py

--- operations.py
+++ operations.py
@@ -57,3 +57,15 @@ class Identity(nn.Module):
 
     def forward(self, x):
         return x
+
+
+class Zero(nn.Module):
+
+  def __init__(self, stride):
+    super(Zero, self).__init__()
+    self.stride = stride
+
+  def forward(self, x):
+    if self.stride == 1:
+      return x.mul(0.)
+    return x[:,:,::self.stride,::self.stride].mul(0.)
